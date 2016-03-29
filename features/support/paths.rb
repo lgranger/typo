e02ae03edee72d233ce004c89573b42d93da0495
Lauren Granger <lr.granger21@gmail.com>
2016-03-29 13:55:12 -0700

    add path support for the admin content edit page

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -17,6 +17,8 @@ module NavigationHelpers
       '/'
     when /^the new article page$/
       '/admin/content/new'
+    when /^the admin content edit page$/i
+      "/admin/content/edit/4"
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
